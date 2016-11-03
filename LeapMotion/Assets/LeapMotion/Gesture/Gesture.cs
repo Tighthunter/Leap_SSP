01fd2c9f1ee0e8d4dde8639091542f3721034539
Bernhard Stöger <bernhardstoeger1@gmail.com>
2016-11-03 16:17:58 +0100

    initial project commit

---
+++ LeapMotion/Assets/LeapMotion/Gesture/Gesture.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap.Unity;
+
+class Gesture : MonoBehaviour {
+    string gestureName;
+    int id;
+    public int[] beats;
+
+    PointingState[] pointingStates;
+
+    public Gesture(string gestureName, int id, int[] beats, PointingState[] states)
+    {
+        this.gestureName = gestureName;
+        this.id = id;
+        this.beats = beats;
+        this.pointingStates = states;
+    }
+}
---
+++ LeapMotion/Assets/Logic.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap.Unity;
+using System.Linq;
+
+class Logic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Gesture stone = new Gesture("Stone", 1, new int[] { 3 }, new PointingState[] { PointingState.NotExtended, PointingState.NotExtended, PointingState.NotExtended, PointingState.NotExtended, PointingState.NotExtended });
+        Gesture paper = new Gesture("Paper", 2, new int[] { 1 }, new PointingState[] { PointingState.Extended, PointingState.Extended, PointingState.Extended, PointingState.Extended, PointingState.Extended });
+        Gesture scissors = new Gesture("Scissors", 3, new int[] { 2 }, new PointingState[] { PointingState.Either, PointingState.Extended, PointingState.Extended, PointingState.NotExtended, PointingState.NotExtended });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public int compareGestures(Gesture player1, Gesture player2)
+    {
+        foreach(var beat in player1.beats)
+        {
+            player2.beats.Where(x => x == beat);
+        }
+        return 0;
+    }
+}
