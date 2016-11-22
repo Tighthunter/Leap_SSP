b04487cffb6c48587300c7142c8d0cf5c69bcfe5
Nico Koecher <if14b026@technikum-wien.at>
2016-11-22 16:54:18 +0100

    added new anim

--- LeapMotion/Assets/Logic/PrimitiveAIPlayer.cs
+++ LeapMotion/Assets/Logic/PrimitiveAIPlayer.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using Assets.Interfaces;
 using UniRx;
+using UnityEngine;
 
 namespace Assets.Logic
 {
-    public class PrimitiveAIPlayer : IPlayer
+    public class PrimitiveAIPlayer : MonoBehaviour , IPlayer
     {
+        private Animation animation;
 
+        //http://answers.unity3d.com/questions/37411/how-can-i-wait-for-an-animation-to-complete.html
+
+        public void Awake()
+        {
+            
+        }
 
         public IObservable<PlayerState> GetPlayerState()
         {
