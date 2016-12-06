ad449673de2d32fdeb127fc670ae0a2bcbb98615
Martin <motch1391@gmail.com>
2016-12-06 18:10:57 +0100

    UI First draft

---
+++ LeapMotion/Assets/Logic/UI/UiSliderEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiSliderEvent : MonoBehaviour
+{
+
+    private Slider slider;
+    private Text text;
+
+	// Use this for initialization
+	void Start ()
+	{
+	   slider = GetComponent<Slider>();
+        text = transform.parent.GetChild(1).GetComponent<Text>();
+        text.text = "Best of " + slider.value;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateText()
+    {
+        text.text = "Best of " + slider.value;
+    }
+}
